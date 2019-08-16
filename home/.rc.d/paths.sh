0c07fb56e35c2124a76f3de51ef1973824845c6e
Richard Heelin <richardh@channelgrabber.com>
2019-08-16 13:55:39 +0100

    Include all bin paths in $PATH

--- home/.rc.d/paths.sh
+++ home/.rc.d/paths.sh
@@ -1,2 +1,9 @@
 # Include my bin folder on path
-export PATH="$HOME/bin:$PATH"
+if [ -d $HOME/bin ]; then
+    export PATH="$HOME/bin:$PATH"
+fi
+
+# Include local bin folder on path
+if [ -d $HOME/.local/bin ]; then
+    export PATH="$HOME/.local/bin:$PATH"
+fi
