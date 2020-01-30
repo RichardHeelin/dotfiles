565e776314fd2aafdcca6a0986dc58ef1c0b1cdb
Richard Heelin <richardh@channelgrabber.com>
2020-01-30 13:42:00 +0000

    Fixed bad path checks

--- home/.rc.d/paths.sh
+++ home/.rc.d/paths.sh
@@ -1,14 +1,14 @@
 # Include my bin folder on path
-if [ -d $HOME/bin ]; then
+if [ -d "$HOME/bin" ]; then
     export PATH="$HOME/bin:$PATH"
 fi
 
 # Include local bin folder on path
-if [ -d $HOME/.local/bin ]; then
+if [ -d "$HOME/.local/bin" ]; then
     export PATH="$HOME/.local/bin:$PATH"
 fi
 
 # Include go bin folder on path
-if [ ! -z $GOPATH ] && [ -d $GOPATH/bin]; then
+if [ ! -z "$GOPATH" ] && [ -d "$GOPATH/bin" ]; then
     export PATH="$GOPATH/bin:$PATH"
 fi
